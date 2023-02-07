3be012bde91ef85c6845d4a7352adb3ca2203c86
nadira30 <namadoumahamane1@cub.uca.edu>
2023-02-07 15:31:50 -0600

    adding fastai cnn

--- train_and_deploy/cnn_network.py
+++ train_and_deploy/cnn_network.py
@@ -7,12 +7,11 @@ image_size = 300
 class cnn_network(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3,6,3)
-        self.conv2 = nn.Conv2d(6,12,3)
+        self.conv1 = nn.Conv2d(1,6,3)
+        self.conv2 = nn.Conv2d(6,16,5)
         self.fc1 = nn.Linear(3*image_size*image_size, 120)
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,2)
-
 
     def forward(self, x):
         x = torch.flatten(x, 1)
@@ -20,3 +19,44 @@ class cnn_network(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+
+class Linear(nn.Module):
+    def __init__(self):
+        #image = 3*160*120 = 57,600
+        super(Linear, self).__init__()
+        self.dropout = 0.1
+        # init the layers
+        # 3, 2, 5*5
+        # 2,
+        self.conv24 = nn.Conv2d(3, 24, kernel_size=(5, 5), stride=(2, 2))
+        self.conv32 = nn.Conv2d(24, 32, kernel_size=(5, 5), stride=(2, 2))
+        self.conv64_5 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=(2, 2))
+        self.conv64_3 = nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
+        # * 5*5
+        self.fc1 = nn.Linear(6656, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.drop = nn.Dropout(self.dropout)
+        self.relu = nn.ReLU()
+        self.output1 = nn.Linear(50, 1)
+        self.output2 = nn.Linear(50, 1)
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        x = self.relu(self.conv24(x))
+        x = self.drop(x)
+        x = self.relu(self.conv32(x))
+        x = self.drop(x)
+        x = self.relu(self.conv64_5(x))
+        x = self.drop(x)
+        x = self.relu(self.conv64_3(x))
+        x = self.drop(x)
+        x = self.relu(self.conv64_3(x))
+        x = self.drop(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x1 = self.drop(x)
+        angle = self.output1(x1)
+        throttle = self.output2(x1)
+        return torch.cat((angle, throttle), 1)
