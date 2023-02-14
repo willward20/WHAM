4231ce1fe876d59fbd7e9ea4d9201f053da8e6bb
linzhanguca <lzhang12@uca.edu>
2023-02-14 17:48:45 -0600

    update train_and_deploy

--- train_and_deploy/autopilot.py
+++ train_and_deploy/autopilot.py
@@ -1,82 +1,84 @@
 #!/usr/bin/python3
 import cv2 as cv
-import servo1 as servo
-import motor
-from torchvision.io import read_image
-from torchvision.transforms import ToTensor, Resize
-import torch
-from cnn_network import cnn_network
 from adafruit_servokit import ServoKit
-import json
+from gpiozero import PhaseEnableMotor
+# from torchvision.io import read_image
+# from torchvision.transforms import ToTensor, Resize
+import torch
+from dense_network import DenseNetwork
 import time
 
-#pca.frequency = 50
-kit = ServoKit(channels=16)
+
+# SETUP
+engine = PhaseEnableMotor(phase=19, enable=26)
+kit = ServoKit(channels=8, address=0x40)
+steer = kit.servo[0]
+MAX_THROTTLE = 0.25
+STEER_CENTER = 90
+MAX_STEER = 50
+assert MAX_THROTTLE <= 1
+steer.angle = 90
 
 
 # Load in configuration constants for throttle and steering
-f = open('config.json')
-data = json.load(f)
-steering_trim = data['steering_trim']
-throttle_lim = data['throttle_trim']
 
 
 # Load CNN model
-model = cnn_network()
-model.load_state_dict(torch.load("indoor_buckets_combined.pth", map_location=torch.device('cpu')))
+model = DenseNetwork()
+model.load_state_dict(torch.load("./model2023-02-14-16-28.pth", map_location=torch.device('cpu')))
 
-# Setup Transforms
-img2tensor = ToTensor()
-resize = Resize(size=(60,80))
-
-# Create video capturer
-cap = cv.VideoCapture(0) #video capture from 0 or -1 should be the first camera plugged in. If passing 1 it would select the second camera
-# cap.set(cv.CAP_PROP_FPS, 10)
-
-times = [] # array to hold the elapsed time between each recieved frame
-start_time = time.time()
-
-while True:
-    ret, frame = cap.read()   
-    if frame is not None:
-        #cv.imshow('frame', frame)  # debug
-        frame = cv.resize(frame, (int(frame.shape[1]), int(frame.shape[0])))
-        img_tensor = img2tensor(frame) # added thi sline to get the colored img 
-        #print(f"frame size: {frame.shape}")
-        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # we changed this from b&w to color
-        #img_tensor = img2tensor(gray)
-        img_tensor = resize(img_tensor) # I am 90% certain that this line is not needed -- changing the size should not affect predictions
-        print(img_tensor.shape)
-    with torch.no_grad():
-        pred = model(img_tensor.unsqueeze(dim=0)) # This line adds an extra dimension to the image tensor (print shape before and after to observe this effect)
-    #print(pred)
-    steering, throttle = pred[0][0].item(), pred[0][1].item()
-    if throttle * throttle_lim < -100:
-        throttle = -1
-    print("steering: ", steering, "     throttle: ", throttle)
-    motor.drive(throttle * throttle_lim) 
-    #print("motor: ", throttle * throttle_lim) 
-    ang = 90 * (1 + steering) + steering_trim 
-    if ang > 180:
-        ang = 180
-    elif ang < 0:
-        ang = 0
-    kit.servo[0].angle = ang
-    #print("ang: ", ang)
-
-    elapsed_time = time.time() - start_time
-    times.append(elapsed_time)
-    start_time = time.time()
-    #print("elapsed time: ", elapsed_time)
-    #print("Average Recieved Image Rate: ", sum(times) / len(times))
-
-    if cv.waitKey(1)==ord('q'):
-        motor.stop()
-        motor.close()
-        break
-
-
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
-        
+# # Setup Transforms
+# img2tensor = ToTensor()
+# resize = Resize(size=(60,80))
+#
+# # Create video capturer
+# cap = cv.VideoCapture(0) #video capture from 0 or -1 should be the first camera plugged in. If passing 1 it would select the second camera
+# # cap.set(cv.CAP_PROP_FPS, 10)
+#
+# times = [] # array to hold the elapsed time between each recieved frame
+# start_time = time.time()
+#
+# while True:
+#     ret, frame = cap.read()   
+#     if frame is not None:
+#         #cv.imshow('frame', frame)  # debug
+#         frame = cv.resize(frame, (int(frame.shape[1]), int(frame.shape[0])))
+#         img_tensor = img2tensor(frame) # added thi sline to get the colored img 
+#         #print(f"frame size: {frame.shape}")
+#         #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # we changed this from b&w to color
+#         #img_tensor = img2tensor(gray)
+#         img_tensor = resize(img_tensor) # I am 90% certain that this line is not needed -- changing the size should not affect predictions
+#         print(img_tensor.shape)
+#     with torch.no_grad():
+#         pred = model(img_tensor.unsqueeze(dim=0)) # This line adds an extra dimension to the image tensor (print shape before and after to observe this effect)
+#     #print(pred)
+#     steering, throttle = pred[0][0].item(), pred[0][1].item()
+#     if throttle * throttle_lim < -100:
+#         throttle = -1
+#     print("steering: ", steering, "     throttle: ", throttle)
+#     motor.drive(throttle * throttle_lim) 
+#     #print("motor: ", throttle * throttle_lim) 
+#     ang = 90 * (1 + steering) + steering_trim 
+#     if ang > 180:
+#         ang = 180
+#     elif ang < 0:
+#         ang = 0
+#     kit.servo[0].angle = ang
+#     #print("ang: ", ang)
+#
+#     elapsed_time = time.time() - start_time
+#     times.append(elapsed_time)
+#     start_time = time.time()
+#     #print("elapsed time: ", elapsed_time)
+#     #print("Average Recieved Image Rate: ", sum(times) / len(times))
+#
+#     if cv.waitKey(1)==ord('q'):
+#         motor.stop()
+#         motor.close()
+#         break
+#
+#
+# # When everything done, release the capture
+# cap.release()
+# cv.destroyAllWindows()
+#         
--- train_and_deploy/collect_data.py
+++ train_and_deploy/collect_data.py
@@ -1,25 +1,41 @@
-#!/usr/bin/python3
+import sys
 import os
-import cv2 as cv
-#from vidstab.VidStab import VidStab
-from adafruit_servokit import ServoKit
-import motor
-import RPi.GPIO as GPIO
-import pygame
-import csv
 from datetime import datetime
 import time
-from gpiozero import LED
-import json
+import numpy as np
+import pygame
+from pygame.locals import *
+from pygame import event, display, joystick
+from adafruit_servokit import ServoKit
+from gpiozero import PhaseEnableMotor
+import cv2 as cv
+import csv
 
-# define servokit
-kit = ServoKit(channels=16)
 
-f = open('config.json')
-data = json.load(f)
-steering_trim = data['steering_trim']
-throttle_lim = data['throttle_trim']
+# SETUP
+engine = PhaseEnableMotor(phase=19, enable=26)
+kit = ServoKit(channels=8, address=0x40)
+steer = kit.servo[0]
+MAX_THROTTLE = 0.25
+STEER_CENTER = 100
+MAX_STEER = 50
+assert MAX_THROTTLE <= 1
+steer.angle = 90
+# init vars
+record_data = True
+speed, ang = 0., 0.
+action = []
+frame_count = 0
 
+# init controller
+display.init()
+joystick.init()
+print(f"{joystick.get_count()} joystick connected")
+js = joystick.Joystick(0)
+# init camera
+cv.startWindowThread()
+cam = cv.VideoCapture(0)
+cam.set(cv.CAP_PROP_FPS, 30)
 # create data storage
 image_dir = 'data' + datetime.now().strftime("%Y-%m-%d-%H-%M") + '/images/'
 if not os.path.exists(image_dir):
@@ -28,73 +44,48 @@ if not os.path.exists(image_dir):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
-# initialize devices
-pygame.display.init()
-pygame.joystick.init()
-pygame.joystick.Joystick(0).init()
-#stabilizer = VidStab()
-cap = cv.VideoCapture(0) #video capture from 0 or -1 should be the first camera plugged in. If passing 1 it would select the second camera
-cap.set(cv.CAP_PROP_FPS, 30)
-i = 0  # image index
-action = [0., 0.]
-Record_data = -1
-led = LED(4)
 
-while True:
-    ret, frame = cap.read()   
-    if frame is not None:
-        #cv.imshow('frame', frame)  # debug
-        #frame = cv.resize(frame, (int(frame.shape[1]), int(frame.shape[0]))) 
-        frame = cv.resize(frame, (300, 300))
-        #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #stabilized_frame = stabilizer.stabilize_frame(input_frame=gray,smoothing_window=4)
-    # if stabilized_frame is None:
-    #     print('no frame')
-    #     break
 
-    #get thorttle and steering values from joysticks.
-    pygame.event.pump()
-    throttle = round((pygame.joystick.Joystick(0).get_axis(1)),2) # between -1 (max reverse) and 1 (max forward), rounded to 2 sig figs 
-    motor.drive(throttle * throttle_lim) # multiplies speed within range -100 to 100 (or whatever throttle_lim is)
-    steer = round((pygame.joystick.Joystick(0).get_axis(3)), 2) # between -1 (left) and 1 (right)
-    ang = 90 * (1 + steer) + steering_trim
-    if ang > 180:
-        ang = 180
-    elif ang < 0:
-        ang = 0
-    kit.servo[15].angle = ang
-    # steer = 90 + steering_trim + steer * 90
-    # servo.turn(steer)
-    # turn(steer)
-    print(throttle*throttle_lim, ang)
-    
-    ##########################################################################################################################################
-    action = [steer, throttle] # this MUST be [steering, throttle] because that's the order that train.py expects (originaly it was reversed)
-    ##########################################################################################################################################
-
-    # print(f"action: {action}") # debug
-    # save image
-    if pygame.joystick.Joystick(0).get_button(0) == 1:
-        Record_data = Record_data * -1
-        if Record_data == 1:
-            print("Recording Data")
-            led.on()
-        else:
-            print("Stopping Data Logging")
-            led.off()
-        time.sleep(0.1)
-    
-    if Record_data == 1:
-        cv.imwrite(image_dir + str(i)+'.jpg', frame) # changed frame to gray
-        # save labels
-        label = [str(i)+'.jpg'] + list(action)
-        label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
-        with open(label_path, 'a+', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(label)  # write the data
-        i += 1
-        
-    if cv.waitKey(1)==ord('q'):
-        break
-    
-        
+# MAIN
+try:
+    while True:
+        ret, frame = cam.read()
+        frame_count += 1
+        for e in event.get():
+            if e.type == QUIT:
+                print("QUIT detected, terminating...")
+                pygame.quit()
+                sys.exit()
+            if e.type == JOYAXISMOTION:
+                v_0 = js.get_axis(0)
+                v_4 = js.get_axis(4)
+                # print(f"steering joystick value: {v_0}, speed joystick value: {v_4}")
+                speed = -np.clip(v_4, -MAX_THROTTLE, MAX_THROTTLE)
+                if speed > 0:
+                    engine.forward(speed)
+                elif speed < 0:
+                    engine.backward(-speed)
+                else:
+                    engine.stop()
+                ang = STEER_CENTER - MAX_STEER * v_0
+                steer.angle = ang
+            action = [ang, speed]
+        if record_data:
+            image = cv.resize(frame, (300, 300))
+            cv.imwrite(image_dir + str(frame_count)+'.jpg', image)  # save frame
+            # save labels
+            label = [str(frame_count)+'.jpg'] + list(action)
+            label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
+            with open(label_path, 'a+', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(label)  # write the data
+        if cv.waitKey(1) == ord('q'):
+            engine.stop()
+            engine.close()
+            cv.destroyAllWindows()
+            break
+       
+except KeyboardInterrupt:
+    engine.stop()
+    engine.close()
+    cv.destroyAllWindows()
