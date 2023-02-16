6a0e926cd32ce006920c71be05c65a7f71920472
linzhanguca <lzhang12@uca.edu>
2023-02-15 20:50:09 -0600

    update collect_data with both labels ranged from -1 to 1

--- drive/collect_data.py
+++ drive/collect_data.py
@@ -17,10 +17,10 @@ engine = PhaseEnableMotor(phase=19, enable=26)
 kit = ServoKit(channels=8, address=0x40)
 steer = kit.servo[0]
 MAX_THROTTLE = 0.32
-STEER_CENTER = 90
+STEER_CENTER = 100
 MAX_STEER = 50
-assert MAX_THROTTLE <= 1
-steer.angle = 90
+engine.stop()
+steer.angle = STEER_CENTER
 # init jotstick controller
 display.init()
 joystick.init()
@@ -38,9 +38,10 @@ if not os.path.exists(image_dir):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
 # init vars
 record_data = True
-speed, ang = 0., 0.
+vel, ang = 0., 0.
 action = []
 frame_count = 0
 
@@ -49,41 +50,54 @@ frame_count = 0
 try:
     while True:
         ret, frame = cam.read()
-        frame_count += 1
+        if ret:  # check camera
+            frame_count += 1
+        else:
+            print("No image received!")
+            engine.stop()
+            engine.close()
+            cv.destroyAllWindows()
+            pygame.quit()
+            sys.exit()
         for e in event.get():
             if e.type == QUIT:
                 print("QUIT detected, terminating...")
+                engine.stop()
+                engine.close()
+                cv.destroyAllWindows()
                 pygame.quit()
                 sys.exit()
             if e.type == JOYAXISMOTION:
-                axval_0 = js.get_axis(0)
-                axval_4 = js.get_axis(4)
-                speed = -np.clip(axval_4, -MAX_THROTTLE, MAX_THROTTLE)
-                if speed > 0:
-                    engine.forward(speed)
-                elif speed < 0:
-                    engine.backward(-speed)
+                ax0_val = js.get_axis(0)
+                ax4_val = js.get_axis(4)
+                vel = -np.clip(ax4_val, -MAX_THROTTLE, MAX_THROTTLE)
+                if vel > 0:  # drive motor
+                    engine.forward(vel)
+                elif vel < 0:
+                    engine.backward(-vel)
                 else:
                     engine.stop()
-                ang = STEER_CENTER - MAX_STEER * axval_0
-                steer.angle = ang
-            action = [speed, ang]
-            print(f"engine speed: {speed}, steering angle: {ang}")
+                ang = STEER_CENTER - MAX_STEER * ax0_val
+                steer.angle = ang  # drive servo
+            action = [ax0_val, ax4_val]
+            print(f"engine speed: {vel}, steering angle: {ang}")
         if record_data:
             image = cv.resize(frame, (300, 300))
-            cv.imwrite(image_dir + str(frame_count)+'.jpg', image)  # save frame
-            label = [str(frame_count)+'.jpg'] + list(action)  # save labels
-            label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
+            cv.imwrite(image_dir + str(frame_count)+'.jpg', image)  # save image
+            label = [str(frame_count)+'.jpg'] + list(action)
             with open(label_path, 'a+', newline='') as f:
                 writer = csv.writer(f)
-                writer.writerow(label)
+                writer.writerow(label)  # save labels
         if cv.waitKey(1) == ord('q'):
             engine.stop()
             engine.close()
             cv.destroyAllWindows()
-            break
-       
+            pygame.quit()
+            sys.exit()
+
 except KeyboardInterrupt:
     engine.stop()
     engine.close()
     cv.destroyAllWindows()
+    pygame.quit()
+    sys.exit()
