8f29a7f582890636f2641f36f2539e38e4c69d74
linzhanguca <lzhang12@uca.edu>
2023-02-15 19:07:47 -0600

    setup train.py

--- README.md
+++ README.md
@@ -4,43 +4,7 @@ Final repository for senior design 2.
 Competiting for the Autonomous Vehicle Challenge held by the National Robotics Challenge in Marion, Ohio.   
 Date: April 13, 14, 15 2023
 
-## Timelapse
-{% include googleDrivePlayer.html id=page.driveId %}
 
-## Requirements
-- Build a fully autonomous and self-contained vehicle
-- Vehicule should naviguate 4 waypoints and cross the finish line under 5min 
-- Vehicle size: 24" * 24" * 24"
-- No transmission or communication except GPS
-- No tether to a laptop or a device 
-- The event runs regardless of the weather
-- Everything necessary for the vehicle naviguation, processing, sensing must be attached
+## TODO
+- [ ] Training labels standardization
 
-## Goals
-- [x] Set up the raspberri pi and software installation
-- [x] Set up Github page
-- [x] Order part list
-- [ ] Mechanical Design
-- [x] Document the competition Requirements
-- [x] Donkey Car software training and model
-- [ ] Build neural networks for the wham car
-- [ ] Compare Performance of Donkey to WHAM! car
-- [X] Build Video Stabilizer
-- [X] Determine and build program for controller type (keyboard or ps4)
-
-## Important Links 
-- [Weekly Reports](https://drive.google.com/drive/u/1/folders/1FO2PocXw_wa_GlBfGjyqvPfI0MiCo4qa) 
-- [National Robotics Challenge](https://www.thenrc.org/)
-- Senior Design 2022 Report
-  - [Trojan Donkey](https://mail.google.com/mail/u/1/#search/zhang/FMfcgzGqQJlfxzFzcpkXsgshXsswKDnJ?projector=1)
-  - [RobotNinjas](https://docs.google.com/document/d/1TyaSFLOAew-xImfiMrTmogvYBtwF_ChW31fuu-zrTaM/edit)
-- https://docs.donkeycar.com/
-
-## Contributors 
-- [Austin Miller](https://github.com/amillertime)
-- [Colby Hoggard](https://github.com/choggard123)
-- [Nadira Amadou](https://github.com/nadira30)
-- [Will Ward](https://github.com/willward20)
-
-## Advisor
-- [Dr. Lin Zhang](https://github.com/linzhangUCA)
--- train/train.py
+++ train/train.py
@@ -28,25 +28,22 @@ class AutopilotDataset(Dataset):
         return image.float(), speed, angle
 
 
-labels_path = "/home/pbd0/playground/wham_buggy/train/data/20230214_2155/labels.csv"
-image_dir = "/home/pbd0/playground/wham_buggy/train/data/20230214_2155/images/"
+labels_path = "/home/pbd0/playground/wham_buggy/train/data/lsca-20230215_1723/labels.csv"
+image_dir = "/home/pbd0/playground/wham_buggy/train/data/lsca-20230215_1723/images/"
 dataset = AutopilotDataset(labels_path, image_dir)
-# print("data length: ", len(dataset))
+print("data length: ", len(dataset))
 # Define the size for train and test data
-train_data_len = len(dataset)
-train_data_size = round(train_data_len*0.9)
-test_data_size = round(train_data_len*0.1)
-# print("len and train and test: ", train_data_len, " ", train_data_size, " ", test_data_size)
+dataset_size = len(dataset)
+train_size = round(dataset_size*0.9)
+test_size = dataset_size - train_size
+print(f"training dataset size: {train_size}, test dataset size: {test_size}")
 # Load the datset (split into train and test)
-train_data, test_data = random_split(dataset, [train_data_size, test_data_size])
+train_data, test_data = random_split(dataset, [train_size, test_size])
 train_dataloader = DataLoader(train_data, batch_size=128)
 test_dataloader = DataLoader(test_data, batch_size=128)
 # image_sample, speed_sample, angle_sample = next(iter(train_dataloader))
 
 
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Using {device} device")
 class DenseNetwork(nn.Module):
     def __init__(self):
         super().__init__()
@@ -64,98 +61,6 @@ class DenseNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-model = DenseNetwork().to(device)
-print(model)
-# DEVICE = torch.device("cuda")
-# class NeuralNetwork(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 3)
-#         self.conv2 = nn.Conv2d(6, 12, 3)
-#         self.fc1 = nn.Linear(3*640*480, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, 2)
-#
-#     def forward(self, x):
-#         x = torch.flatten(x, 1)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-#
-#
-# def train(dataloader, model, loss_fn, optimizer):
-#     num_batches = len(dataloader.dataset)
-#     model.train()
-#     
-#     train_loss = 0.0
-#
-#
-#     for batch, (X, steering, throttle) in enumerate(dataloader):
-#         #Combine steering and throttle into one tensor (2 columns, X rows)
-#         y = torch.stack((steering, throttle), -1) 
-#         #y = y.float()
-#
-#         X, y = X.to(DEVICE), y.to(DEVICE)
-#         #print("Size X: ", X.size()) # torch.Size([BATCHSIZE, 3, 480, 640])
-#
-#         # Compute prediction error
-#         pred = model(X)  # forward propagation
-#         loss = loss_fn(pred, y)  # compute loss
-#         optimizer.zero_grad()  # zero previous gradient
-#         loss.backward()  # back propagatin
-#         optimizer.step()  # update parameters
-#         
-#         #if batch % 10 == 0:
-#         #    loss, current = loss.item(), batch * len(X)
-#         #    print(f"Train Loss: {loss:>7f}")
-#         train_loss += loss.item()
-#     #print("Average train loss: ", statistics.mean(train_loss))
-#     return train_loss/num_batches
-#
-#         
-# # Define a test function to evaluate model performance
-# def test(dataloader, model, loss_fn):
-#     #size = len(dataloader.dataset)
-#     num_batches = len(dataloader)
-#     model.eval()
-#     test_loss = 0.0
-#     with torch.no_grad():
-#         for X, steering, throttle in dataloader:
-#
-#             #Combine steering and throttle into one tensor (2 columns, X rows)
-#             y = torch.stack((steering, throttle), -1) 
-#             y = y.float()
-#             
-#             X, y = X.to(DEVICE), y.to(DEVICE)
-#
-#             pred = model(X)
-#             loss = loss_fn(pred, y).item()
-#             #print(f"Test Loss: {loss:>7f}")
-#             test_loss += loss
-#             #accuracy += (pred.argmax(1) == y).type(torch.float).sum().item()
-#     #avg_loss = statistics.mean(test_loss)
-#     #print(f"Test Error: Avg loss: {avg_loss:>8f} \n")
-#
-#     return test_loss/num_batches
-#
-#
-# # Graph the test and train data
-# def graph_data(x, train, test, TITLE, FILENAME):
-#
-#     fig = plt.figure()
-#     axs = fig.add_subplot(1,1,1)
-#
-#     plt.plot(x, train, color='r', label="Training Loss")
-#     plt.plot(x, test, color='b', label='Testing Loss')
-#     axs.set_ylabel('Loss')
-#     axs.set_xlabel('Training Epoch')
-#     axs.set_title(TITLE)
-#     axs.legend()
-#     fig.savefig(FILENAME)
-#
-#     return
-
 
 ############################################################################
 #           M         M        A        IIIIIII     N        N             #
@@ -164,45 +69,60 @@ print(model)
 #           M    M    M     A     A        I        N      N N             # 
 #           M         M    A       A    IIIIIII     N        N             #
 ############################################################################
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using {device} device")
 
 
-# Create a dataset
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    epoch_loss = 0.
+    for batch, (image, speed, angle) in enumerate(dataloader):
+        target = torch.stack((speed, angle), -1)
+        X, y = image.to(device), target.to(device)
+        # Compute prediction error
+        pred = model(X)
+        batch_loss = loss_fn(pred, y)
+        # Backpropagation
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+        batch_loss, sample_count = batch_loss.item(), (batch + 1) * len(X)
+        epoch_loss = (epoch_loss*batch + batch_loss) / (batch + 1)
+        # if batch % 10 == 0:
+        print(f"loss: {batch_loss:>7f}  [{sample_count:>5d}/{size:>5d}]")
 
-# Initialize the model
-# epochs = 5
-# model = NeuralNetwork().to(DEVICE)
-# loss_fn = nn.MSELoss()
-# optimizer = torch.optim.Adam(model.parameters(), lr= 0.0001)
-#
-# # Optimize the model
-# train_loss = []
-# test_loss = []
-# for t in range(epochs):
-#     print(f"Epoch {t+1}\n-------------------------------")
-#     training_loss = train(train_dataloader, model, loss_fn, optimizer)
-#     testing_loss = test(test_dataloader, model, loss_fn)
-#     print("average training loss: ", training_loss)
-#     print("average testing loss: ", testing_loss)
-#     # save values
-#     train_loss.append(training_loss)
-#     test_loss.append(testing_loss)   
-#
-# print(f"Optimize Done!")
-#
-#
-# #print("final test lost: ", test_loss[-1])
-# len_train_loss = len(train_loss)
-# len_test_loss = len(test_loss)
-# print("Train loss length: ", len_train_loss)
-# print("Test loss length: ", len_test_loss)
-#
-#
-# # create array for x values for plotting train
-# epochs_array = list(range(1, epochs+1))
-# print(epochs_array)
-#
-# graph_data(epochs_array, train_loss, test_loss, "TEST", "test.jpg")
-#
+    return epoch_loss
+
+
+def test(dataloader, model, loss_fn):
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for image, speed, angle in dataloader:
+            target = torch.stack((speed, angle), -1)
+            X, y = image.to(device), target.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+    test_loss /= num_batches
+    print(f"Test Error: {test_loss:>8f} \n")
+
+
+model = DenseNetwork().to(device)
+print(model)
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+epochs = 50
+losses = []
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    epoch_loss = train(train_dataloader, model, loss_fn, optimizer)
+    losses.append(epoch_loss)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+plt.plot(losses)
+plt.show()
 #
 # """
 # # Load an image from the dataset and make a prediction
@@ -215,6 +135,7 @@ print(model)
 # print(pred)
 # """
 #
-# # Save the model
-# torch.save(model.state_dict(), "TEST.pth")
-# print("Saved PyTorch Model State to TEST.pth")
+# Save the model
+model_path = "/home/pbd0/playground/wham_buggy/train/models/lsca-20230215_1723.pth"
+torch.save(model.state_dict(), model_path)
+print(f"Saved autopilot model state to {model_path}")
