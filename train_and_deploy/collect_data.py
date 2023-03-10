f3732cbf6467c355947b16c886d0caf67510d6f6
wham pi <wham@pi.com>
2023-03-10 14:45:24 -0600

    steering trim

--- train_and_deploy/collect_data.py
+++ train_and_deploy/collect_data.py
@@ -47,17 +47,6 @@ label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.c
 # init controller
 pygame.display.init()
 pygame.joystick.init()
-<<<<<<< HEAD
-pygame.joystick.Joystick(0).init()
-#stabilizer = VidStab()
-cap = cv.VideoCapture(0) #video capture from 0 or -1 should be the first camera plugged in. If passing 1 it would select the second camera
-cap.set(cv.CAP_PROP_FPS, 30)
-i = 0  # image index
-action = [0., 0.]
-Record_data = -1
-led = LED(4)
-headlight = LED(16)
-=======
 js = pygame.joystick.Joystick(0)
 # init variables
 throttle, steer = 0., 0.
@@ -73,59 +62,8 @@ for i in reversed(range(60)):  # warm up camera
 # init timer, uncomment if you are cuious about frame rate
 start_stamp = time()
 ave_frame_rate = 0.
->>>>>>> f1895eb38957ffd1f482227c9af79b61807c156b
 
 
-<<<<<<< HEAD
-    #get thorttle and steering values from joysticks.
-    pygame.event.pump()
-    throttle = round((pygame.joystick.Joystick(0).get_axis(1)),2) # between -1 (max reverse) and 1 (max forward), rounded to 2 sig figs 
-    motor.drive(throttle * throttle_lim) # multiplies speed within range -100 to 100 (or whatever throttle_lim is)
-    steer = round((pygame.joystick.Joystick(0).get_axis(3)), 2) # between -1 (left) and 1 (right)
-    ang = 90 * (1 + steer) + steering_trim
-    if ang > 180:
-        ang = 180
-    elif ang < 0:
-        ang = 0
-    kit.servo[15].angle = ang
-    # steer = 90 + steering_trim + steer * 90
-    # servo.turn(steer)
-    # turn(steer)
-    print(throttle*throttle_lim, ang)
-    
-    ##########################################################################################################################################
-    action = [steer, throttle] # this MUST be [steering, throttle] because that's the order that train.py expects (originaly it was reversed)
-    ##########################################################################################################################################
-
-    # print(f"action: {action}") # debug
-    # save image
-    if pygame.joystick.Joystick(0).get_button(0) == 1:
-        Record_data = Record_data * -1
-        if Record_data == 1:
-            print("Recording Data")
-            led.on()
-            headlight.on()
-        else:
-            print("Stopping Data Logging")
-            led.off()
-            headlight.off()
-        time.sleep(0.1)
-    
-    if Record_data == 1:
-        cv.imwrite(image_dir + str(i)+'.jpg', frame) # changed frame to gray
-        # save labels
-        label = [str(i)+'.jpg'] + list(action)
-        label_path = os.path.join(os.path.dirname(os.path.dirname(image_dir)), 'labels.csv')
-        with open(label_path, 'a+', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(label)  # write the data
-        i += 1
-        
-    if cv.waitKey(1)==ord('q'):
-        break
-    
-        
-=======
 # MAIN
 try:
     while True:
@@ -181,4 +119,3 @@ except KeyboardInterrupt:
     cv.destroyAllWindows()
     pygame.quit()
     sys.exit()
->>>>>>> f1895eb38957ffd1f482227c9af79b61807c156b
