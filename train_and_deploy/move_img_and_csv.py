913c7c433133eb45b3a8049422352bd6cda5d7a3
lab_computer <lab_computer>
2023-03-11 15:19:37 -0600

    combined folder 202303031347 and 202303031349 into data_finale_120_160

--- train_and_deploy/move_img_and_csv.py
+++ train_and_deploy/move_img_and_csv.py
@@ -3,11 +3,15 @@ import uuid
 import cv2 as cv
 
 import pandas as pd
-train_and_deploy/data/202303031349
-old_csv = '../data/202303031349/labels.csv'
-images = '../data/202303031349//images'
-new_csv = "../data/data_finale/labels.csv"
 
+#file processed
+# 202303031349 -->done
+# 202303031347 --> done
+# 202303031421 --> not process those are images collected in the robotics club room for training. 
+
+old_csv = 'data/202303031421/labels.csv'
+images = 'data/202303031421/images'
+new_csv = "data/data_finale/labels1.csv"
 
 def move_img(filename):
     unique_id = str(uuid.uuid4())
@@ -15,16 +19,16 @@ def move_img(filename):
     unique_id = unique_id[4]
     new_filename = unique_id + ".jpg"
     img_path = f"{images}/{filename}"
-    # print(img_path)
+    #print(img_path)
     image = cv.imread(img_path)
-    cv.imwrite(f"data_finale/img/{new_filename}", image)
+    cv.imwrite(f"data/data_finale/img1/{new_filename}", image)
     return new_filename
 
 
 # reading the csv file
 df = pd.read_csv(old_csv, header=None)
 
-for filename in os.listdir(images):
+for filename in os.listdir(images):             
     if filename.endswith('.jpg'):
         id = move_img(filename)
         count = 0
